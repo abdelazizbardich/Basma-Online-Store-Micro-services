4b0dbd9aa0d730c5bc05f0ddbf21e5d2ee4e8712
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-05-18 01:41:06 +0100

    :sparkles: Ajout des projets spring boot pour les microservices back-end :sparkles:

---
+++ Services/Products/src/main/java/com/basmaonlinestore/productsserver/HelloController.java
@@ -0,0 +1,14 @@
+package com.basmaonlinestore.productsserver;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World, Spring Boot!";
+    }
+
+}
---
+++ Services/auth/src/main/java/com/basmaonlinestore/authorisationserver/MyWebApplication.java
@@ -0,0 +1,13 @@
+package com.basmaonlinestore.authorisationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebApplication.class, args);
+    }
+
+}
