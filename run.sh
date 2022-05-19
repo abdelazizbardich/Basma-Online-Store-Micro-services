a38da0ec2ef1864f9df5a4c4aacb11cbb264aa40
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-05-19 23:47:35 +0100

    📝 commenting image remover codes from run.sh 📝

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 docker-compose down -v && \
-docker rmi basma_api_gateway:v1 || true && \
-docker rmi basma_auth_service:v1 || true && \
-docker rmi basma_front_service:v1 || true && \
-docker rmi basma_orders_service:v1 || true && \
-docker rmi basma_products_service:v1 || true && \
-docker rmi basma_users_service:v1 || true && \
+# docker rmi basma_api_gateway:v1 || true && \
+# docker rmi basma_auth_service:v1 || true && \
+# docker rmi basma_front_service:v1 || true && \
+# docker rmi basma_orders_service:v1 || true && \
+# docker rmi basma_products_service:v1 || true && \
+# docker rmi basma_users_service:v1 || true && \
 docker-compose up -d
